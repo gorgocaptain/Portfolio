48581592196b1d5997834ad6a017dacc22de9cb8
gorgocaptain <gorgocaptain@gmail.com>
2024-10-01 21:11:42 -0400

    cool beans

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // Importing createBrowserRouter and RouterProvider
 import Beats from './Components/Beats';
 import Me from './Components/Me';
 import Posts from './Components/Posts';
 import Contact from './Components/Contact';
-import App from './App';
+import App from './App'; // Importing App
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App />, // Using App here
   },
   {
     path: "/Beats",
@@ -38,7 +35,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} /> {/* Passing the router to RouterProvider */}
   </React.StrictMode>
 );
 
